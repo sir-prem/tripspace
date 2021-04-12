a333aa5f7c39e5131830cc60ee91cde5fb8e4941
Prem Gangadharan <pgangad@deakin.edu.au>
2021-04-13 01:53:15 +1000

    user login working, & logo / materialise css added

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ app.use(express.urlencoded({
 //routes
 var test = require('./test.js');
 var userProf = require('./user-profile.js');
-var newListing = require('./new-listing.js');
+var driverProf = require('./driver-profile.js');
 
 //both index.js and e.g. user-profile.js should be in same directory
 app.use('/test', test);
 app.use('/user-profile', userProf);
-app.use('/new-listing', newListing);
+app.use('/driver-profile', driverProf);
 
 http.listen(port,()=>{
 	console.log("Listening on port ", port);
