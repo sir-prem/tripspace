2e7d96c3970b10e22c3a958ee5dbcd00cfe4cbc6
mc paul jr <msoriano@deakin.edu.au>
2021-06-01 14:35:42 +1000

    added map of victoria using mapbox  and text where the driver currently is

---
+++ views/js/map.js
@@ -0,0 +1,10 @@
+
+mapboxgl.accessToken='pk.eyJ1IjoibXNvcmlhbm8yNCIsImEiOiJja29oeHhkNmgwcGxwMnhsbDFqMDl3eTMyIn0.D84IIpYoQgfRVUL-9M-M1g';
+	
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v9',
+  center: [144.9350287575595, -37.78287455478831],
+  zoom: 11
+});
+
