15e18b5150562fab468f85776f82bd956c9a8016
mc paul jr <msoriano@deakin.edu.au>
2021-06-01 14:53:48 +1000

    added marker image and put  markers on the map

--- views/js/map.js
+++ views/js/map.js
@@ -8,3 +8,57 @@ var map = new mapboxgl.Map({
   zoom: 11
 });
 
+var geojson = {
+    type: 'FeatureCollection',
+    features: [
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [ 144.9236254016745, -37.77631930256852]
+        },
+        properties: {
+            title: 'User 1',
+            description: '7 West St, Ascot Vale VIC 3032'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [144.9470863818076, -37.805161928627605]
+        },
+        properties: {
+            title: 'User 2',
+            description: '45 Silk Pl, West Melbourne VIC 3003'
+        }
+    },
+    {
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            coordinates: [144.91886918164963, -37.75670591199163]
+        },
+        properties: {
+            title: 'Driver',
+            description: '884 Mt Alexander Rd, Moonee Ponds VIC 3039'
+        }
+    }]
+}
+    
+
+
+//add markers to map
+geojson.features.forEach(function(marker){
+				
+    //create  a HTML for each feature
+    var el = document.createElement('div');
+    el.className = 'marker';
+
+    //make a marker for each feature and add to the map
+    new mapboxgl.Marker(el)
+    .setLngLat(marker.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+    .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
+    .addTo(map);
+});
