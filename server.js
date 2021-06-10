ad93f942d8a7882ae9bcb43f5a7d5939da33bdba
Prem Gangadharan <pgangad@deakin.edu.au>
2021-06-11 03:41:26 +1000

    minor change to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const Booking = require('./models/booking')
+const Booking = require('./Models/booking')
 const socketio = require('socket.io');
 const http = require('http');
 const server = http.createServer(app);
